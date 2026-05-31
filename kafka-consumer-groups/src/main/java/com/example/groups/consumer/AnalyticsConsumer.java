dd870b97418475d576c5e1ab4aba76d74c41df81
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-05-31 20:34:14 +0530

    Update AnalyticsConsumer.java

--- kafka-consumer-groups/src/main/java/com/example/groups/consumer/AnalyticsConsumer.java
+++ kafka-consumer-groups/src/main/java/com/example/groups/consumer/AnalyticsConsumer.java
@@ -2,10 +2,13 @@ package com.example.groups.consumer;
 import com.example.groups.dto.OrderRequest;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
 @Component
 public class AnalyticsConsumer {
+ 
  @KafkaListener(topics="orders", groupId="analytics-group")
  public void consume(OrderRequest order){
   System.out.println("analytics-group received order="+order.orderId());
  }
+ 
 }
