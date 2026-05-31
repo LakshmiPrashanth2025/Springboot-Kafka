916a00265cbea8f8276ed469a9ed651e3e1cfacc
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-05-31 20:39:40 +0530

    Update OrderRequest.java

--- kafka-consumer-groups/src/main/java/com/example/groups/dto/OrderRequest.java
+++ kafka-consumer-groups/src/main/java/com/example/groups/dto/OrderRequest.java
@@ -1,5 +1,6 @@
 package com.example.order.dto;
 
+//For records, Java automatically generates accessor methods for each field with the same name as the field
 public record OrderRequest(
   String orderId,
   String customerId,
