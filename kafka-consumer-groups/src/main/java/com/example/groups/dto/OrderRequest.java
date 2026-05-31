656af6703e349d5e4268556f4765529448aeb587
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-05-31 20:36:34 +0530

    Update OrderRequest.java

--- kafka-consumer-groups/src/main/java/com/example/groups/dto/OrderRequest.java
+++ kafka-consumer-groups/src/main/java/com/example/groups/dto/OrderRequest.java
@@ -1,2 +1,10 @@
 package com.example.order.dto;
-public record OrderRequest(String orderId,String customerId,String product,double amount){}
+
+public record OrderRequest(
+  String orderId,
+  String customerId,
+  String product,
+  double amount)
+ {
+  
+  }
