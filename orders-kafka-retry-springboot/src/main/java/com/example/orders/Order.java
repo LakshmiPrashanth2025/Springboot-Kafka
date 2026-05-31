86a1b559cd4badc8c2e3e277e48859d8cd6b2f3d
LakshmiPrashanth2025 <lakshmi.prashant@gmail.com>
2026-05-31 20:59:29 +0530

    Update Order.java

--- orders-kafka-retry-springboot/src/main/java/com/example/orders/Order.java
+++ orders-kafka-retry-springboot/src/main/java/com/example/orders/Order.java
@@ -5,10 +5,26 @@ public class Order {
   private String item;
   private int quantity;
 
-  public String getOrderId(){ return orderId; }
-  public void setOrderId(String orderId){ this.orderId = orderId; }
-  public String getItem(){ return item; }
-  public void setItem(String item){ this.item = item; }
-  public int getQuantity(){ return quantity; }
-  public void setQuantity(int quantity){ this.quantity = quantity; }
+  public String getOrderId(){ 
+    return orderId; 
+  }
+  
+  public void setOrderId(String orderId){ 
+    this.orderId = orderId; 
+  }
+  
+  public String getItem(){ 
+    return item; 
+  }
+  public void setItem(String item){ 
+    this.item = item; 
+  }
+  
+  public int getQuantity(){ 
+    return quantity; 
+  }
+  
+  public void setQuantity(int quantity){ 
+    this.quantity = quantity; 
+  }
 }
